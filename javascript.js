1a6def5004c0488835e8fd975ab503bac339cd75
ahgr3y <leehh97@gmail.com>
2024-12-18 11:03:47 +0800

    Make add/delete button work

--- javascript.js
+++ javascript.js
@@ -1,5 +1,30 @@
+const counter = document.querySelector("#counter");
 const amountToChange = document.querySelector("#amount-to-change");
 const incrementer = document.querySelector("#incrementer");
+const decrementer = document.querySelector("#decrementer");
 
-incrementer.addEventListener("click", event => console.log(amountToChange.value));
-console.log(amountToChange.value);
+incrementer.addEventListener("click", (event) => {
+    adjustCounter(amountToChange, "inc");
+});
+
+decrementer.addEventListener("click", (event) => {
+    adjustCounter(amountToChange, "dec");
+});
+
+function adjustCounter(valueString, type) {
+    const counterNum = Number(counter.textContent);
+    const adjustBy = Number(amountToChange.value);
+
+    let newCounter = 0;
+
+    if (type === "inc") {
+        newCounter = counterNum + adjustBy;
+    } else if (type === "dec") {
+        newCounter = counterNum - adjustBy;
+        if (newCounter < 0) {
+            newCounter = 0;
+        }
+    }
+
+    counter.textContent = newCounter;
+}
