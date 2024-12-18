1bb7a70612222327dea660eecb483ae073bfffa1
ahgr3y <leehh97@gmail.com>
2024-12-18 10:13:15 +0800

    Add framework

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-console.log("Slot Machine Jackpot");
+const amountToChange = document.querySelector("#amount-to-change");
+const incrementer = document.querySelector("#incrementer");
+
+incrementer.addEventListener("click", event => console.log(amountToChange.value));
+console.log(amountToChange.value);
