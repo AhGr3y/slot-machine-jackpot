067d31fada633ec31c8a2ea2097f7e1b7088898f
ahgr3y <leehh97@gmail.com>
2024-12-18 16:23:21 +0800

    Add slot machine effect for slot 1

--- javascript.js
+++ javascript.js
@@ -1,16 +1,47 @@
+const tokens = [
+    "✊",
+    "🖐️",
+    "✌️",
+];
+
 const counter = document.querySelector("#counter");
+const hitButton = document.querySelector("#hit-button");
 const amountToChange = document.querySelector("#amount-to-change");
 const incrementer = document.querySelector("#incrementer");
 const decrementer = document.querySelector("#decrementer");
+const numOfTokens = 32;
 
+let slotNumber = 1;
+let slot = document.querySelector(`#slot-${slotNumber}`);
+let slotTokensHeight = 0;
+
+// Clicking the 'Hit!' button will spin one slot at a time
+hitButton.addEventListener("click", (event) => {
+    fillElement(slot, tokens, numOfTokens);
+    slot.animate(
+        [
+            { transform: "translateY(0)" },
+            { transform: `translateY(-${slotTokensHeight - (slotTokensHeight * 2 / numOfTokens)}px)` },
+        ],
+        {
+            duration: 6000,
+            easing: "cubic-bezier(0.42, 0, 0.1, 1.0)",
+            fill: "forwards",
+        },
+    )
+});
+
+// Button to add amount to jackpot
 incrementer.addEventListener("click", (event) => {
     adjustCounter(amountToChange, "inc");
 });
 
+// Button to remove amount from jackpot
 decrementer.addEventListener("click", (event) => {
     adjustCounter(amountToChange, "dec");
 });
 
+// Used by incrementer/decrementer to adjust jackpot amount
 function adjustCounter(valueString, type) {
     const counterNum = Number(counter.textContent);
     const adjustBy = Number(amountToChange.value);
@@ -27,4 +58,23 @@ function adjustCounter(valueString, type) {
     }
 
     counter.textContent = newCounter;
+}
+
+// Append children to element with items of an
+// array randomly, and update slotTokensHeight.
+function fillElement(element, array, numOfChild) {
+    for (let i = 0; i < numOfChild; i++) {
+        const randomItem = getRandomItem(array);
+        const div = document.createElement("div");
+        div.textContent = randomItem;
+        div.style.fontSize = "150px";
+        div.style.textAlign = "center";
+        element.appendChild(div);
+        slotTokensHeight += div.clientHeight;
+    }
+}
+
+function getRandomItem(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
 }
